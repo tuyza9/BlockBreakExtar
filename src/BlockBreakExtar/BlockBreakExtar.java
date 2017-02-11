92b4bfc57e87f8b28cfb322030b9837a8f38b087
tuyza9 <tuyza9@hotmail.com>
2017-02-11 12:40:29 +0700

    BBE
    BlockBreakExtar v.2

--- src/BlockBreakExtar/BlockBreakExtar.java
+++ src/BlockBreakExtar/BlockBreakExtar.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package BlockBreakExtar;
 
 import java.io.File;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -31,25 +26,24 @@ public class BlockBreakExtar
 extends JavaPlugin implements Listener
 {
     
-    FileConfiguration conf;
-    File cfile;
+    public static FileConfiguration conf;
+    public static File cfile;
     
     public void onEnable(){
         
         conf = getConfig();
         conf.options().copyDefaults(true);
-        saveConfig();
+        saveDefaultConfig();
         cfile = new File(getDataFolder(), "config.yml");
         
         Bukkit.getLogger().info(getDescription() + " be has enable version: " + getDescription().getVersion());
         PluginManager manager = this.getServer().getPluginManager();
-        manager.registerEvents((Listener) new Events(),this);
         manager.registerEvents((Listener) new Commands(),this);
         regCmd();
+        manager.registerEvents((Listener) new Events(),this);
     }
     public void onDisable(){
         Bukkit.getServer().getPluginManager().disablePlugins();
-        
     }
     
     public void regCmd(){
@@ -62,115 +56,125 @@ extends JavaPlugin implements Listener
         public void BlockExp(BlockExpEvent e){
             
            Block b = e.getBlock();
-           if(b.getType() == Material.getMaterial(conf.getString("block-type-drop-exp-1"))){
-               e.setExpToDrop(conf.getInt("blcok-set-exp-drop-1"));
+           Location l = e.getBlock().getLocation();
+           
+           //Block drop exp
+           if(new Location(l.getWorld(), l.getX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("block-drop-exp-id-1")){
+               e.setExpToDrop(conf.getInt("block-set-exp-drop-1"));
            }
-           if(b.getType() == Material.getMaterial(conf.getString("block-type-drop-exp-2"))){
-               e.setExpToDrop(conf.getInt("blcok-set-exp-drop-2"));
+           if(new Location(l.getWorld(), l.getX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("block-drop-exp-id-2")){
+               e.setExpToDrop(conf.getInt("block-set-exp-drop-2"));
            }
-           if(b.getType() == Material.getMaterial(conf.getString("block-type-drop-exp-3"))){
-               e.setExpToDrop(conf.getInt("blcok-set-exp-drop-3"));
+           if(new Location(l.getWorld(), l.getX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("block-drop-exp-id-3")){
+               e.setExpToDrop(conf.getInt("block-set-exp-drop-3"));
            }
-           if(b.getType() == Material.getMaterial(conf.getString("block-type-drop-exp-4"))){
-               e.setExpToDrop(conf.getInt("blcok-set-exp-drop-4"));
+           if(new Location(l.getWorld(), l.getX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("block-drop-exp-id-4")){
+               e.setExpToDrop(conf.getInt("block-set-exp-drop-4"));
            }
-           if(b.getType() == Material.getMaterial(conf.getString("block-type-drop-exp-5"))){
-               e.setExpToDrop(conf.getInt("blcok-set-exp-drop-5"));
+           if(new Location(l.getWorld(), l.getX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("block-drop-exp-id-5")){
+               e.setExpToDrop(conf.getInt("block-set-exp-drop-5"));
            }
-           if(b.getType() == Material.getMaterial(conf.getString("block-type-drop-exp-6"))){
-               e.setExpToDrop(conf.getInt("blcok-set-exp-drop-6"));
+           if(new Location(l.getWorld(), l.getX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("block-drop-exp-id-6")){
+               e.setExpToDrop(conf.getInt("block-set-exp-drop-6"));
            }
-           if(b.getType() == Material.getMaterial(conf.getString("block-type-drop-exp-7"))){
-               e.setExpToDrop(conf.getInt("blcok-set-exp-drop-7"));
+           if(new Location(l.getWorld(), l.getX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("block-drop-exp-id-7")){
+               e.setExpToDrop(conf.getInt("block-set-exp-drop-7"));
            }
-           if(b.getType() == Material.getMaterial(conf.getString("block-type-drop-exp-8"))){
-               e.setExpToDrop(conf.getInt("blcok-set-exp-drop-8"));
+           if(new Location(l.getWorld(), l.getX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("block-drop-exp-id-8")){
+               e.setExpToDrop(conf.getInt("block-set-exp-drop-8"));
            }
-           if(b.getType() == Material.getMaterial(conf.getString("block-type-drop-exp-9"))){
-               e.setExpToDrop(conf.getInt("blcok-set-exp-drop-9"));
+           if(new Location(l.getWorld(), l.getX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("block-drop-exp-id-9")){
+               e.setExpToDrop(conf.getInt("block-set-exp-drop-9"));
            }
-           if(b.getType() == Material.getMaterial(conf.getString("block-type-drop-exp-10"))){
-               e.setExpToDrop(conf.getInt("blcok-set-exp-drop-10"));
+           if(new Location(l.getWorld(), l.getX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("block-drop-exp-id-10")){
+               e.setExpToDrop(conf.getInt("block-set-exp-drop-10"));
            }
+           
+           
         }
+        
         public void EventBlockBreak(BlockBreakEvent e){
             Player p = e.getPlayer();
             Block b = e.getBlock();
+            Location l = e.getBlock().getLocation();
              
-             if(b.getType() == Material.getMaterial(conf.getString("no-break-1"))){
-                e.setCancelled(true);
-                p.sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+            //No Break Block
+             if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("no-break-1")){
+                 e.setCancelled(true);
+                 (p).sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+             }
+             if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("no-break-2")){
+                 e.setCancelled(true);
+                 (p).sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+             }
+             if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("no-break-3")){
+                 e.setCancelled(true);
+                 (p).sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+             }
+             if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("no-break-4")){
+                 e.setCancelled(true);
+                 (p).sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+             }
+             if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("no-break-5")){
+                 e.setCancelled(true);
+                 (p).sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+             }
+             if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("no-break-6")){
+                 e.setCancelled(true);
+                 (p).sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+             }
+             if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("no-break-7")){
+                 e.setCancelled(true);
+                 (p).sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+             }
+             if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("no-break-8")){
+                 e.setCancelled(true);
+                 (p).sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+             }
+             if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("no-break-9")){
+                 e.setCancelled(true);
+                 (p).sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+             }
+             if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("no-break-10")){
+                 e.setCancelled(true);
+                 (p).sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+             }
+             
+            //Bast Block Player get commands
+            if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("bast-block-type-command-1")){
+                (p).performCommand(conf.getString("bast-block-command-1"));
             }
-            if(b.getType() == Material.getMaterial(conf.getString("no-break-2"))){
-                e.setCancelled(true);
-                p.sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+            if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("bast-block-type-command-2")){
+                (p).performCommand(conf.getString("bast-block-command-2"));
             }
-            if(b.getType() == Material.getMaterial(conf.getString("no-break-3"))){
-                e.setCancelled(true);
-                p.sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+            if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("bast-block-type-command-3")){
+                (p).performCommand(conf.getString("bast-block-command-3"));
             }
-            if(b.getType() == Material.getMaterial(conf.getString("no-break-4"))){
-                e.setCancelled(true);
-                p.sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+            if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("bast-block-type-command-4")){
+                (p).performCommand(conf.getString("bast-block-command-4"));
             }
-            if(b.getType() == Material.getMaterial(conf.getString("no-break-5"))){
-                e.setCancelled(true);
-                p.sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+            if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("bast-block-type-command-5")){
+                (p).performCommand(conf.getString("bast-block-command-5"));
             }
-            if(b.getType() == Material.getMaterial(conf.getString("no-break-6"))){
-                e.setCancelled(true);
-                p.sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+            if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("bast-block-type-command-6")){
+                (p).performCommand(conf.getString("bast-block-command-6"));
             }
-            if(b.getType() == Material.getMaterial(conf.getString("no-break-7"))){
-                e.setCancelled(true);
-                p.sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+            if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("bast-block-type-command-7")){
+                (p).performCommand(conf.getString("bast-block-command-7"));
             }
-            if(b.getType() == Material.getMaterial(conf.getString("no-break-8"))){
-                e.setCancelled(true);
-                p.sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+            if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("bast-block-type-command-8")){
+                (p).performCommand(conf.getString("bast-block-command-8"));
             }
-            if(b.getType() == Material.getMaterial(conf.getString("no-break-9"))){
-                e.setCancelled(true);
-                p.sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+            if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("bast-block-type-command-9")){
+                (p).performCommand(conf.getString("bast-block-command-9"));
             }
-            if(b.getType() == Material.getMaterial(conf.getString("no-break-10"))){
-                e.setCancelled(true);
-                p.sendMessage(ChatColor.DARK_RED + "You can not beat Brock this.");
+            if(new Location(l.getWorld(), l.getBlockX(), l.getBlockY(), l.getBlockZ()).getBlock().getTypeId() == conf.getInt("bast-block-type-command-10")){
+                (p).performCommand(conf.getString("bast-block-command-10"));
             }
             
-            //block break commands
-            if(b.getType() == Material.getMaterial(conf.getString("block-break-type-cmd-1"))){
-                (p).performCommand(conf.getString("block-break-commands-1"));
-            }
-            if(b.getType() == Material.getMaterial(conf.getString("block-break-type-cmd-2"))){
-                (p).performCommand(conf.getString("block-break-commands-2"));
-            }
-            if(b.getType() == Material.getMaterial(conf.getString("block-break-type-cmd-3"))){
-                (p).performCommand(conf.getString("block-break-commands-3"));
-            }
-            if(b.getType() == Material.getMaterial(conf.getString("block-break-type-cmd-4"))){
-                (p).performCommand(conf.getString("block-break-commands-4"));
-            }
-            if(b.getType() == Material.getMaterial(conf.getString("block-break-type-cmd-5"))){
-                (p).performCommand(conf.getString("block-break-commands-5"));
-            }
-            if(b.getType() == Material.getMaterial(conf.getString("block-break-type-cmd-6"))){
-                (p).performCommand(conf.getString("block-break-commands-6"));
-            }
-            if(b.getType() == Material.getMaterial(conf.getString("block-break-type-cmd-7"))){
-                (p).performCommand(conf.getString("block-break-commands-7"));
-            }
-            if(b.getType() == Material.getMaterial(conf.getString("block-break-type-cmd-8"))){
-                (p).performCommand(conf.getString("block-break-commands-8"));
-            }
-            if(b.getType() == Material.getMaterial(conf.getString("block-break-type-cmd-9"))){
-                (p).performCommand(conf.getString("block-break-commands-9"));
-            }
-            if(b.getType() == Material.getMaterial(conf.getString("block-break-type-cmd-10"))){
-                (p).performCommand(conf.getString("block-break-commands-10"));
-            }
-            }
         }
+    
+    }
     
     public class Commands implements CommandExecutor,Listener{
 
@@ -186,7 +190,7 @@ extends JavaPlugin implements Listener
             }
             if(cmd.getName().equalsIgnoreCase("bbereload")){
                 conf = YamlConfiguration.loadConfiguration(cfile);
-                Sender.sendMessage(ChatColor.DARK_GREEN + "Reload succss");
+                Sender.sendMessage(ChatColor.GREEN + "Reload succss");
             }
                     }
                     if(args.length > 0){
@@ -200,6 +204,7 @@ extends JavaPlugin implements Listener
             }
             return true;
         }
+    }
+    
+    }
         
-    }
-    }
